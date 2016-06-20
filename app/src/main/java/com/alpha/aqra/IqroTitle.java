9fddc536dcee070ffb46f4e086cbf8eb52c03723
ilhamyudo <ilhamyudo27@gmail.com>
2016-06-20 12:18:13 +0700

    tambah getset Title

---
+++ app/src/main/java/com/alpha/aqra/IqroTitle.java
@@ -0,0 +1,21 @@
+package com.alpha.aqra;
+
+/**
+ * Created by IlhamWindows10 on 6/20/2016.
+ */
+public class IqroTitle {
+
+    String title;
+
+    public IqroTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
