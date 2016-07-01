0a7cfab67914e08e4ebb8fac642d6f262ac67ea6
alphaenciel <alphaenciel@gmail.com>
2016-07-01 10:01:00 +0700

    Coba Metode Iqro 2

--- app/src/main/java/com/alpha/aqra/GridFragment2.java
+++ app/src/main/java/com/alpha/aqra/GridFragment2.java
@@ -22,6 +22,7 @@ public class GridFragment2 extends Fragment {
     private GridAdapter2 mGridAdapter2;
     GridItems2[] GridItems2 = {};
     private Activity activity;
+    SoundIqro2 soundIqro2;
 
 
     public GridFragment2(GridItems2[] GridItems2, Activity activity) {
@@ -61,7 +62,90 @@ public class GridFragment2 extends Fragment {
     }
 
     public void onGridItemClick(GridView g, View v, int position, long id) {
-
+        if (GridItems2[position].title=="1"){soundIqro2.sound_bawa.start();}
+        else if (GridItems2[position].title=="2"){soundIqro2.sound_baro.start();}
+        else if (GridItems2[position].title=="3"){soundIqro2.sound_tada.start();}
+        else if (GridItems2[position].title=="4"){soundIqro2.sound_tata.start();}
+        else if (GridItems2[position].title=="5"){soundIqro2.sound_tajai.start();}
+        else if (GridItems2[position].title=="6"){soundIqro2.sound_taro.start();}
+        else if (GridItems2[position].title=="7"){soundIqro2.sound_badza.start();}
+        else if (GridItems2[position].title=="8"){soundIqro2.sound_baba.start();}
+        else if (GridItems2[position].title=="9"){soundIqro2.sound_jaitsa.start();}
+        else if (GridItems2[position].title=="10"){soundIqro2.sound_tsatsa.start();}
+        else if (GridItems2[position].title=="11"){soundIqro2.sound_tadza.start();}
+        else if (GridItems2[position].title=="12"){soundIqro2.sound_batsa.start();}
+        else if (GridItems2[position].title=="13"){soundIqro2.sound_baro.start();}
+        else if (GridItems2[position].title=="14"){soundIqro2.sound_badza.start();}
+        else if (GridItems2[position].title=="15"){soundIqro2.sound_tada.start();}
+        else if (GridItems2[position].title=="16"){soundIqro2.sound_taro.start();}
+        else if (GridItems2[position].title=="17"){soundIqro2.sound_taata.start();}
+        else if (GridItems2[position].title=="18"){soundIqro2.sound_taa.start();}
+        else if (GridItems2[position].title=="19"){soundIqro2.sound_baata.start();}
+        else if (GridItems2[position].title=="20"){soundIqro2.sound_baa.start();}
+        else if (GridItems2[position].title=="21"){soundIqro2.sound_jaitsa.start();}
+        else if (GridItems2[position].title=="22"){soundIqro2.sound_baa.start();}
+        else if (GridItems2[position].title=="23"){soundIqro2.sound_badza.start();}
+        else if (GridItems2[position].title=="24"){soundIqro2.sound_taa.start();}
+        else if (GridItems2[position].title=="25"){soundIqro2.sound_saain.start();}
+        else if (GridItems2[position].title=="26"){soundIqro2.sound_kana.start();}
+        else if (GridItems2[position].title=="27"){soundIqro2.sound_zama.start();}
+        else if (GridItems2[position].title=="28"){soundIqro2.sound_tokha.start();}
+        else if (GridItems2[position].title=="29"){soundIqro2.sound_banana.start();}
+        else if (GridItems2[position].title=="30"){soundIqro2.sound_nababa.start();}
+        else if (GridItems2[position].title=="31"){soundIqro2.sound_natana.start();}
+        else if (GridItems2[position].title=="32"){soundIqro2.sound_nanana.start();}
+        else if (GridItems2[position].title=="33"){soundIqro2.sound_nataba.start();}
+        else if (GridItems2[position].title=="34"){soundIqro2.sound_tabana.start();}
+        else if (GridItems2[position].title=="35"){soundIqro2.sound_banata.start();}
+        else if (GridItems2[position].title=="36"){soundIqro2.sound_nabata.start();}
+        else if (GridItems2[position].title=="37"){soundIqro2.sound_nabadza.start();}
+        else if (GridItems2[position].title=="38"){soundIqro2.sound_badzana.start();}
+        else if (GridItems2[position].title=="39"){soundIqro2.sound_banaro.start();}
+        else if (GridItems2[position].title=="40"){soundIqro2.sound_robana.start();}
+        else if (GridItems2[position].title=="41"){soundIqro2.sound_wanadza.start();}
+        else if (GridItems2[position].title=="42"){soundIqro2.sound_dzakhaba.start();}
+        else if (GridItems2[position].title=="43"){soundIqro2.sound_nadzaro.start();}
+        else if (GridItems2[position].title=="44"){soundIqro2.sound_badaro.start();}
+        else if (GridItems2[position].title=="45"){soundIqro2.sound_dzakhaba.start();}
+        else if (GridItems2[position].title=="46"){soundIqro2.sound_badaro.start();}
+        else if (GridItems2[position].title=="47"){soundIqro2.sound_wanadza.start();}
+        else if (GridItems2[position].title=="48"){soundIqro2.sound_nadzaro.start();}
+        else if (GridItems2[position].title=="49"){soundIqro2.sound_nabaa.start();}
+        else if (GridItems2[position].title=="50"){soundIqro2.sound_nabaalif.start();}
+        else if (GridItems2[position].title=="51"){soundIqro2.sound_naroain.start();}
+        else if (GridItems2[position].title=="52"){soundIqro2.sound_dzaroha.start();}
+        else if (GridItems2[position].title=="53"){soundIqro2.sound_nawafa.start();}
+        else if (GridItems2[position].title=="54"){soundIqro2.sound_wanaa.start();}
+        else if (GridItems2[position].title=="55"){soundIqro2.sound_tatsaba.start();}
+        else if (GridItems2[position].title=="56"){soundIqro2.sound_tsabata.start();}
+        else if (GridItems2[position].title=="57"){soundIqro2.sound_yanaro.start();}
+        else if (GridItems2[position].title=="58"){soundIqro2.sound_ronaya.start();}
+        else if (GridItems2[position].title=="59"){soundIqro2.sound_yabaro.start();}
+        else if (GridItems2[position].title=="60"){soundIqro2.sound_robaya.start();}
+        else if (GridItems2[position].title=="61"){soundIqro2.sound_danaya.start();}
+        else if (GridItems2[position].title=="62"){soundIqro2.sound_yadana.start();}
+        else if (GridItems2[position].title=="63"){soundIqro2.sound_nabaya.start();}
+        else if (GridItems2[position].title=="64"){soundIqro2.sound_bayana.start();}
+        else if (GridItems2[position].title=="65"){soundIqro2.sound_yadaya.start();}
+        else if (GridItems2[position].title=="66"){soundIqro2.sound_nadzaro.start();}
+        else if (GridItems2[position].title=="67"){soundIqro2.sound_najaila.start();}
+        else if (GridItems2[position].title=="68"){soundIqro2.sound_dzakhaba.start();}
+        else if (GridItems2[position].title=="69"){soundIqro2.sound_bayana.start();}
+        else if (GridItems2[position].title=="70"){soundIqro2.sound_yabaro.start();}
+        else if (GridItems2[position].title=="71"){soundIqro2.sound_danaya.start();}
+        else if (GridItems2[position].title=="72"){soundIqro2.sound_yadana.start();}
+        else if (GridItems2[position].title=="73"){soundIqro2.sound_ayata.start();}
+        else if (GridItems2[position].title=="74"){soundIqro2.sound_yaaba.start();}
+        else if (GridItems2[position].title=="75"){soundIqro2.sound_tsanaa.start();}
+        else if (GridItems2[position].title=="76"){soundIqro2.sound_atsatsa.start();}
+        else if (GridItems2[position].title=="77"){soundIqro2.sound_wanada.start();}
+        else if (GridItems2[position].title=="78"){soundIqro2.sound_ataya.start();}
+        else if (GridItems2[position].title=="79"){soundIqro2.sound_nadhoro.start();}
+        else if (GridItems2[position].title=="80"){soundIqro2.sound_rojaiqo.start();}
+        else if (GridItems2[position].title=="81"){soundIqro2.sound_yasaro.start();}
+        else if (GridItems2[position].title=="82"){soundIqro2.sound_sayala.start();}
+        else if (GridItems2[position].title=="83"){soundIqro2.sound_fataya.start();}
+        else if (GridItems2[position].title=="84"){soundIqro2.sound_yayaya.start();}
         Toast.makeText(
                 activity,
                 "Position Clicked: - " + position + " & " + "Text is: - "
---
+++ app/src/main/java/com/alpha/aqra/SoundIqro2.java
@@ -0,0 +1,84 @@
+package com.alpha.aqra;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by alphaenciel on 7/1/2016.
+ */
+public class SoundIqro2 {
+    public static MediaPlayer sound_baa,sound_baata,sound_baba,sound_badza,sound_baro,sound_batsa,sound_bawa,sound_jaitsa,sound_kana,sound_saain
+            ,sound_taa,sound_taata,sound_tada,sound_tadza,sound_tajai,sound_taro,sound_tata,sound_tokha,sound_tsatsa,sound_zama
+            ,sound_badaro,sound_badzana,sound_banana,sound_banaro,sound_banata,sound_dzakhaba,sound_dzaroha,sound_nabaa,sound_nabaalif
+            ,sound_nababa,sound_nabadza,sound_nabata,sound_nadzaro,sound_nanana,sound_naroain,sound_nataba,sound_natana,sound_nawafa
+            ,sound_robana,sound_tabana,sound_tatsaba,sound_tsabata,sound_wanaa,sound_wanadza,sound_ataya,sound_atsatsa,sound_ayata,sound_bayana
+            ,sound_danaya,sound_fataya,sound_nabaya,sound_nadhoro,sound_najaila,sound_robaya,sound_rojaiqo,sound_ronaya,sound_sayala,sound_tsanaa
+            ,sound_wanada,sound_yaaba,sound_yabaro,sound_yadana,sound_yadaya,sound_yanaro,sound_yasaro,sound_yayaya;
+    public static void SoundPlayer1(Context ctx, int raw_id){sound_baa = MediaPlayer.create(ctx, raw_id);sound_baa.setVolume(100, 100);}
+    public static void SoundPlayer2(Context ctx,int raw_id){sound_baata = MediaPlayer.create(ctx, raw_id);sound_baata.setVolume(100, 100);}
+    public static void SoundPlayer3(Context ctx,int raw_id){sound_baba = MediaPlayer.create(ctx, raw_id);sound_baba.setVolume(100, 100);}
+    public static void SoundPlayer4(Context ctx,int raw_id){sound_badza = MediaPlayer.create(ctx, raw_id);sound_badza.setVolume(100, 100);}
+    public static void SoundPlayer5(Context ctx,int raw_id){sound_baro = MediaPlayer.create(ctx, raw_id);sound_baro.setVolume(100, 100);}
+    public static void SoundPlayer6(Context ctx,int raw_id){sound_batsa = MediaPlayer.create(ctx, raw_id);sound_batsa.setVolume(100, 100);}
+    public static void SoundPlayer7(Context ctx,int raw_id){sound_bawa = MediaPlayer.create(ctx, raw_id);sound_bawa.setVolume(100, 100);}
+    public static void SoundPlayer8(Context ctx,int raw_id){sound_jaitsa = MediaPlayer.create(ctx, raw_id);sound_jaitsa.setVolume(100, 100);}
+    public static void SoundPlayer9(Context ctx,int raw_id){sound_kana = MediaPlayer.create(ctx, raw_id);sound_kana.setVolume(100, 100);}
+    public static void SoundPlayer10(Context ctx,int raw_id){sound_saain = MediaPlayer.create(ctx, raw_id);sound_saain.setVolume(100, 100);}
+    public static void SoundPlayer11(Context ctx,int raw_id){sound_taa = MediaPlayer.create(ctx, raw_id);sound_taa.setVolume(100, 100);}
+    public static void SoundPlayer12(Context ctx,int raw_id){sound_taata = MediaPlayer.create(ctx, raw_id);sound_taata.setVolume(100, 100);}
+    public static void SoundPlayer13(Context ctx,int raw_id){sound_tada = MediaPlayer.create(ctx, raw_id);sound_tada.setVolume(100, 100);}
+    public static void SoundPlayer14(Context ctx,int raw_id){sound_tadza = MediaPlayer.create(ctx, raw_id);sound_tadza.setVolume(100, 100);}
+    public static void SoundPlayer15(Context ctx,int raw_id){sound_tajai = MediaPlayer.create(ctx, raw_id);sound_tajai.setVolume(100, 100);}
+    public static void SoundPlayer16(Context ctx,int raw_id){sound_taro = MediaPlayer.create(ctx, raw_id);sound_taro.setVolume(100, 100);}
+    public static void SoundPlayer17(Context ctx,int raw_id){sound_tata = MediaPlayer.create(ctx, raw_id);sound_tata.setVolume(100, 100);}
+    public static void SoundPlayer18(Context ctx,int raw_id){sound_tokha = MediaPlayer.create(ctx, raw_id);sound_tokha.setVolume(100, 100);}
+    public static void SoundPlayer19(Context ctx,int raw_id){sound_tsatsa = MediaPlayer.create(ctx, raw_id);sound_tsatsa.setVolume(100, 100);}
+    public static void SoundPlayer20(Context ctx,int raw_id){sound_zama = MediaPlayer.create(ctx, raw_id);sound_zama.setVolume(100, 100);}
+    public static void SoundPlayer21(Context ctx,int raw_id){sound_badaro = MediaPlayer.create(ctx, raw_id);sound_badaro.setVolume(100, 100);}
+    public static void SoundPlayer22(Context ctx,int raw_id){sound_badzana = MediaPlayer.create(ctx, raw_id);sound_badzana.setVolume(100, 100);}
+    public static void SoundPlayer23(Context ctx,int raw_id){sound_banana = MediaPlayer.create(ctx, raw_id);sound_banana.setVolume(100, 100);}
+    public static void SoundPlayer24(Context ctx,int raw_id){sound_banaro = MediaPlayer.create(ctx, raw_id);sound_banaro.setVolume(100, 100);}
+    public static void SoundPlayer25(Context ctx,int raw_id){sound_banata = MediaPlayer.create(ctx, raw_id);sound_banata.setVolume(100, 100);}
+    public static void SoundPlayer26(Context ctx,int raw_id){sound_dzakhaba = MediaPlayer.create(ctx, raw_id);sound_dzakhaba.setVolume(100, 100);}
+    public static void SoundPlayer27(Context ctx,int raw_id){sound_dzaroha = MediaPlayer.create(ctx, raw_id);sound_dzaroha.setVolume(100, 100);}
+    public static void SoundPlayer28(Context ctx,int raw_id){sound_nabaa = MediaPlayer.create(ctx, raw_id);sound_nabaa.setVolume(100, 100);}
+    public static void SoundPlayer29(Context ctx,int raw_id){sound_nabaalif = MediaPlayer.create(ctx, raw_id);sound_nabaalif.setVolume(100, 100);}
+    public static void SoundPlayer30(Context ctx,int raw_id){sound_nababa = MediaPlayer.create(ctx, raw_id);sound_nababa.setVolume(100, 100);}
+    public static void SoundPlayer31(Context ctx, int raw_id){sound_nabadza = MediaPlayer.create(ctx, raw_id);sound_nabadza.setVolume(100, 100);}
+    public static void SoundPlayer32(Context ctx,int raw_id){sound_nabata = MediaPlayer.create(ctx, raw_id);sound_nabata.setVolume(100, 100);}
+    public static void SoundPlayer33(Context ctx,int raw_id){sound_nadzaro = MediaPlayer.create(ctx, raw_id);sound_nadzaro.setVolume(100, 100);}
+    public static void SoundPlayer34(Context ctx,int raw_id){sound_nanana = MediaPlayer.create(ctx, raw_id);sound_nanana.setVolume(100, 100);}
+    public static void SoundPlayer35(Context ctx,int raw_id){sound_naroain = MediaPlayer.create(ctx, raw_id);sound_naroain.setVolume(100, 100);}
+    public static void SoundPlayer36(Context ctx,int raw_id){sound_nataba = MediaPlayer.create(ctx, raw_id);sound_nataba.setVolume(100, 100);}
+    public static void SoundPlayer37(Context ctx,int raw_id){sound_natana = MediaPlayer.create(ctx, raw_id);sound_natana.setVolume(100, 100);}
+    public static void SoundPlayer38(Context ctx,int raw_id){sound_nawafa = MediaPlayer.create(ctx, raw_id);sound_nawafa.setVolume(100, 100);}
+    public static void SoundPlayer39(Context ctx,int raw_id){sound_robana = MediaPlayer.create(ctx, raw_id);sound_robana.setVolume(100, 100);}
+    public static void SoundPlayer40(Context ctx,int raw_id){sound_tabana = MediaPlayer.create(ctx, raw_id);sound_tabana.setVolume(100, 100);}
+    public static void SoundPlayer41(Context ctx, int raw_id){sound_tatsaba = MediaPlayer.create(ctx, raw_id);sound_tatsaba.setVolume(100, 100);}
+    public static void SoundPlayer42(Context ctx,int raw_id){sound_tsabata = MediaPlayer.create(ctx, raw_id);sound_tsabata.setVolume(100, 100);}
+    public static void SoundPlayer43(Context ctx,int raw_id){sound_wanaa = MediaPlayer.create(ctx, raw_id);sound_wanaa.setVolume(100, 100);}
+    public static void SoundPlayer44(Context ctx,int raw_id){sound_wanadza = MediaPlayer.create(ctx, raw_id);sound_wanadza.setVolume(100, 100);}
+    public static void SoundPlayer45(Context ctx,int raw_id){sound_ataya = MediaPlayer.create(ctx, raw_id);sound_ataya.setVolume(100, 100);}
+    public static void SoundPlayer46(Context ctx,int raw_id){sound_atsatsa = MediaPlayer.create(ctx, raw_id);sound_atsatsa.setVolume(100, 100);}
+    public static void SoundPlayer47(Context ctx,int raw_id){sound_ayata = MediaPlayer.create(ctx, raw_id);sound_ayata.setVolume(100, 100);}
+    public static void SoundPlayer48(Context ctx,int raw_id){sound_bayana = MediaPlayer.create(ctx, raw_id);sound_bayana.setVolume(100, 100);}
+    public static void SoundPlayer49(Context ctx,int raw_id){sound_danaya = MediaPlayer.create(ctx, raw_id);sound_danaya.setVolume(100, 100);}
+    public static void SoundPlayer50(Context ctx,int raw_id){sound_fataya = MediaPlayer.create(ctx, raw_id);sound_fataya.setVolume(100, 100);}
+    public static void SoundPlayer51(Context ctx, int raw_id){sound_nabaya = MediaPlayer.create(ctx, raw_id);sound_nabaya.setVolume(100, 100);}
+    public static void SoundPlayer52(Context ctx,int raw_id){sound_nadhoro = MediaPlayer.create(ctx, raw_id);sound_nadhoro.setVolume(100, 100);}
+    public static void SoundPlayer53(Context ctx,int raw_id){sound_najaila = MediaPlayer.create(ctx, raw_id);sound_najaila.setVolume(100, 100);}
+    public static void SoundPlayer54(Context ctx,int raw_id){sound_robaya = MediaPlayer.create(ctx, raw_id);sound_robaya.setVolume(100, 100);}
+    public static void SoundPlayer55(Context ctx,int raw_id){sound_rojaiqo = MediaPlayer.create(ctx, raw_id);sound_rojaiqo.setVolume(100, 100);}
+    public static void SoundPlayer56(Context ctx,int raw_id){sound_ronaya = MediaPlayer.create(ctx, raw_id);sound_ronaya.setVolume(100, 100);}
+    public static void SoundPlayer57(Context ctx,int raw_id){sound_sayala = MediaPlayer.create(ctx, raw_id);sound_sayala.setVolume(100, 100);}
+    public static void SoundPlayer58(Context ctx,int raw_id){sound_tsanaa = MediaPlayer.create(ctx, raw_id);sound_tsanaa.setVolume(100, 100);}
+    public static void SoundPlayer59(Context ctx,int raw_id){sound_wanada = MediaPlayer.create(ctx, raw_id);sound_wanada.setVolume(100, 100);}
+    public static void SoundPlayer60(Context ctx,int raw_id){sound_yaaba = MediaPlayer.create(ctx, raw_id);sound_yaaba.setVolume(100, 100);}
+    public static void SoundPlayer61(Context ctx, int raw_id){sound_yabaro = MediaPlayer.create(ctx, raw_id);sound_yabaro.setVolume(100, 100);}
+    public static void SoundPlayer62(Context ctx,int raw_id){sound_yadana = MediaPlayer.create(ctx, raw_id);sound_yadana.setVolume(100, 100);}
+    public static void SoundPlayer63(Context ctx,int raw_id){sound_yadaya = MediaPlayer.create(ctx, raw_id);sound_yadaya.setVolume(100, 100);}
+    public static void SoundPlayer64(Context ctx,int raw_id){sound_yanaro = MediaPlayer.create(ctx, raw_id);sound_yanaro.setVolume(100, 100);}
+    public static void SoundPlayer65(Context ctx,int raw_id){sound_yasaro = MediaPlayer.create(ctx, raw_id);sound_yasaro.setVolume(100, 100);}
+    public static void SoundPlayer66(Context ctx,int raw_id){sound_yayaya = MediaPlayer.create(ctx, raw_id);sound_yayaya.setVolume(100, 100);}
+
+}
